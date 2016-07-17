5b9c0884dff5dafe4696eab5c4f73ad9fe2fa46f
Dmitry Khlystov <dkhlystov@gmail.com>
2016-07-18 01:21:25 +1000

    Some text translations added

--- messages/ru-RU/uploadimage.php
+++ messages/ru-RU/uploadimage.php
@@ -5,4 +5,7 @@ return [
 	'The format of files {files} is not supported.' => 'Формат файлов {files} не поддерживается.',
 	'An error occurred while uploading files {files}.' => 'Возникла ошибка при загрузке файлов {files}.',
 	'You can upload up to {maxCount} files. The files {files} were not uploaded.' => 'Вы можете загрузить не более {maxCount} файлов. Файлы {files} не были загружены.',
+	'Rotate' => 'Повернуть',
+	'Crop' => 'Обрезать',
+	'Remove' => 'Удалить',
 ];
