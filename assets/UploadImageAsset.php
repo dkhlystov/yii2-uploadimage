8fa8b0d1c8e48cbbe17a89c2ca8863b214552725
Dmitry Khlystov <dkhlystov@gmail.com>
2016-07-26 15:41:20 +1000

    PHP 5.4 compatibility

--- assets/UploadImageAsset.php
+++ assets/UploadImageAsset.php
@@ -5,16 +5,27 @@ use yii\web\AssetBundle;
 
 class UploadImageAsset extends AssetBundle
 {
+
 	public $sourcePath = '@uploadimage/assets/uploadimage';
+
 	public $css = [
 		'uploadimage.css',
 	];
+
 	public $js = [
-		'uploadimage' . (YII_DEBUG ? '' : '.min') . '.js',
 		'jquery.form.min.js',
 	];
+
 	public $depends = [
 		'yii\web\JqueryAsset',
 		'uploadimage\assets\FontAwesomeAsset'
 	];
+
+	public function init()
+	{
+		parent::init();
+
+		$this->js[] = 'uploadimage' . (YII_DEBUG ? '' : '.min') . '.js';
+	}
+
 }
